1cdc615c3e869abaeb02f9126e4112ae654874c5
Marek Petr <marek.p4@email.cz>
2020-07-27 21:12:12 +0200

    Basic tetromino movement with keys is complete - added neccesary functions for this purpose

--- script.js
+++ script.js
@@ -63,6 +63,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
       timerID = setInterval(moveDown, 1000)
 
+      //assign functions to keycode
+      function control(e) {
+        //move left
+        if (e.keyCode === 37) {
+          moveLeft()
+        //move right
+        } else if (e.keyCode === 39) {
+          moveRight()  
+        //rotate tetromino
+        } else if (e.keyCode === 38) {
+          rotate()
+        //move down
+        } else if (e.keyCode === 40) {
+          moveDown()
+        }
+      }
+
+      document.addEventListener('keydown', control)
+
       //move down function
       function moveDown() {
         undraw()
@@ -92,7 +111,42 @@ document.addEventListener('DOMContentLoaded', () => {
         if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
           currentPosition += 1
         }
-
         draw()
       }
+
+      function moveRight() {
+        undraw()
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === -1)
+
+        if(!isAtRightEdge) currentPosition += 1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+          currentPosition -= 1
+        }
+        draw()
+      }
+
+      function rotate() {
+        undraw()
+        currentRotation++
+        if(currentRotation === current.length) {
+          currentRotation = 0
+        }
+        current = theTetrominoes[random][currentRotation]
+        draw()
+      }
+
+      //show up-next tetromino in mini-grid display
+      const displaySquaeres = document.querySelectorAll('.mini-grid div')
+      const displayWidth = 4
+      let displayIndex = 0
+
+      //the Tetrominos without rotations
+      const upNextTetrominoes = [
+        [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
+        [0, displayWidth. displayWidth+1, displayWidth*2+1], //zTetromino
+        [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
+        [0, 1, displayWidth, displayWidth+1], //oTetromino
+        [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
+      ]
     })
